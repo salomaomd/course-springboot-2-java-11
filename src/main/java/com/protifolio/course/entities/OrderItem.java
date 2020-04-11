98675a92f5bec3525e005978bb5213d174beb1f2
Salomao <salomaomd@gmail.com>
2020-04-11 14:53:09 -0300

    methods

--- src/main/java/com/protifolio/course/entities/OrderItem.java
+++ src/main/java/com/protifolio/course/entities/OrderItem.java
@@ -47,6 +47,10 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSubTotal() {
+		return price * quatity;
+	}
 
 	@Override
 	public int hashCode() {
